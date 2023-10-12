d5372d17c6f0334d50d23d7a802e64d79411fca0
rreact <77273501+rreact@users.noreply.github.com>
2023-10-13 00:10:47 +0530

    basic form with validations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Home Page</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
